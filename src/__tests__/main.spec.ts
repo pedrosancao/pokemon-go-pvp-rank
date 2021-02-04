c35ebe635233b6f922474581ae64660da3967a74
Pedro Sanção <pedrosancao@users.noreply.github.com>
2021-02-04 00:25:13 -0300

    Fix tests

--- src/__tests__/main.spec.ts
+++ src/__tests__/main.spec.ts
@@ -22,46 +22,48 @@ describe('general-test', () => {
       healthProduct: 122,
       product: 1603558.3516946794,
       cp: 1484,
-      rank: 1191
+      rank: 1191,
     });
     expect(rank).toHaveProperty('rank');
-    expect(rank.rank).toEqual(expect.arrayContaining([
-      {
-        attackStat: 0,
-        defenseStat: 14,
-        healthStat: 11,
-        level: 18.5,
-        attackProduct: 134.4491759754,
-        defenseProduct: 99.4004591613,
-        healthProduct: 125,
-        product: 1670538.7282266484,
-        cp: 1500,
-        rank: 1
-      },
-      {
-        attackStat: 0,
-        defenseStat: 11,
-        healthStat: 15,
-        level: 18.5,
-        attackProduct: 134.4491759754,
-        defenseProduct: 97.676751777,
-        healthProduct: 127,
-        product: 1667834.9661231602,
-        cp: 1500,
-        rank: 2
-      },
-      {
-        attackStat: 0,
-        defenseStat: 15,
-        healthStat: 9,
-        level: 18.5,
-        attackProduct: 134.4491759754,
-        defenseProduct: 99.97502828940002,
-        healthProduct: 124,
-        product: 1666753.461281765,
-        cp: 1497,
-        rank: 3
-      }
-    ]));
+    expect(rank.rank).toEqual(
+      expect.arrayContaining([
+        {
+          attackStat: 0,
+          defenseStat: 14,
+          healthStat: 11,
+          level: 18.5,
+          attackProduct: 134.4491759754,
+          defenseProduct: 99.4004591613,
+          healthProduct: 125,
+          product: 1670538.7282266484,
+          cp: 1500,
+          rank: 1,
+        },
+        {
+          attackStat: 0,
+          defenseStat: 11,
+          healthStat: 15,
+          level: 18.5,
+          attackProduct: 134.4491759754,
+          defenseProduct: 97.676751777,
+          healthProduct: 127,
+          product: 1667834.9661231602,
+          cp: 1500,
+          rank: 2,
+        },
+        {
+          attackStat: 0,
+          defenseStat: 15,
+          healthStat: 9,
+          level: 18.5,
+          attackProduct: 134.4491759754,
+          defenseProduct: 99.97502828940002,
+          healthProduct: 124,
+          product: 1666753.461281765,
+          cp: 1497,
+          rank: 3,
+        },
+      ]),
+    );
   });
 });
