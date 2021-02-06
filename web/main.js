670a25bd605e69a174da678c64ec3c432db1aac1
Pedro Sanção <pedrosancao@users.noreply.github.com>
2021-02-06 00:35:49 -0300

    Create base layout and funcionality

--- web/main.js
+++ web/main.js
@@ -5,15 +5,38 @@ import {
   calculateRank,
 } from '../dist/index.js';
 
-const pokemonName = 'Machamp';
-const pokedexEntry = getByName({ name: pokemonName });
-const rank = calculateRank({
-  pokedexEntry,
-  refAttackStat: 15,
-  refDefenseStat: 15,
-  refHealthStat: 15,
-  maxCP: 1500,
-  maxLevel: 40,
-  minimumStatValue: 0,
+const datalist = document.getElementById('pokedex');
+pokedex.forEach(({ name }) => {
+  datalist.append(new Option(name, name));
 });
-console.log(rank);
+
+document.querySelectorAll('[data-sync]').forEach(el => {
+  el.addEventListener('input', ({ target: { value, dataset } }) => {
+    const syncTarget = document.querySelector(dataset.sync);
+    if (syncTarget) {
+      syncTarget.value = value;
+    }
+  });
+});
+
+document.getElementById('calculate').addEventListener('click', () => {
+  const formData = {};
+  document.querySelectorAll('input[name]').forEach(({ name, value }) => {
+    formData[name] = value;
+  });
+  try {
+    const pokedexEntry = getByName({ name: formData.pokemon });
+    const { rank, occurence } = calculateRank({
+      pokedexEntry,
+      refAttackStat: parseInt(formData.attack, 10),
+      refDefenseStat: parseInt(formData.defense, 10),
+      refHealthStat: parseInt(formData.health, 10),
+      maxCP: 1500,
+      maxLevel: 40,
+      minimumStatValue: 0,
+    });
+    console.log(occurence, rank);
+  } catch (error) {
+    console.log(error);
+  }
+});
